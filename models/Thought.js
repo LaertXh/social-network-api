a862aca99af4da7eeb7b456a61d1eaab33a9dd82
Laert Xhumari <laertxhumari@gmail.com>
2023-04-04 19:54:12 -0400

    fixed bugs related to user routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,36 +1,39 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new Schema({
+const thoughtSchema = new Schema(
+  {
     thoughtText: {
       type: String,
       required: true,
       minlength: 1,
-      maxlength: 280
+      maxlength: 280,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: timestamp => dateFormat(timestamp)
+      get: (timestamp) => dateFormat(timestamp),
     },
     username: {
       type: String,
-      required: true
+      required: true,
     },
-    reactions: [reactionSchema]
+    reactions: [reactionSchema],
   },
-   {
+  {
     toJSON: {
       virtuals: true,
-      getters: true
+      getters: true,
     },
-    id: false
-  });
-  
-  // Create a virtual called 'reactionCount' that retrieves the length of the 'reactions' array field
-  thoughtSchema.virtual('reactionCount').get(function() {
-    return this.reactions.length;
-  });
-  
-const Thought = model('Thought', thoughtSchema);
+    id: false,
+  }
+);
+
+// Create a virtual called 'reactionCount' that retrieves the length of the 'reactions' array field
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
 
 module.exports = Thought;
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,22 +1,22 @@
-const router = require('express').Router();
-const {
-  getVideos,
-  getSingleVideo,
-  createVideo,
-  updateVideo,
-  deleteVideo,
-  addVideoResponse,
-  removeVideoResponse,
-} = require('../../controllers/thoughtsController');
+const router = require("express").Router();
+// const {
+//   getVideos,
+//   getSingleVideo,
+//   createVideo,
+//   updateVideo,
+//   deleteVideo,
+//   addVideoResponse,
+//   removeVideoResponse,
+// } = require('../../controllers/thoughtsController');
 
-// /api/thoughts
-router.route('/').get(getVideos).post(createVideo);
+// // /api/thoughts
+// router.route('/').get(getVideos).post(createVideo);
 
-// /api/thoughts/:thoughtId/reactions
-router
-  .route('/:thoughtId/reactions')
-  .get(getSingleVideo)
-  .put(updateVideo)
-  .delete(deleteVideo);
+// // /api/thoughts/:thoughtId/reactions
+// router
+//   .route('/:thoughtId/reactions')
+//   .get(getSingleVideo)
+//   .put(updateVideo)
+//   .delete(deleteVideo);
 
 module.exports = router;
